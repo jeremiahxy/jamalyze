a79f04076ae5380a7620dee5b9c2f1dfa23339ac
Emily Nassi <emily.nassi1@gmail.com>
2018-03-24 14:00:10 -0400

    map updates

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,7 +1,7 @@
 var mapbox = "https://api.mapbox.com/styles/v1/enassi/cjeojkdsr6ghf2sp346vuxyl9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZW5hc3NpIiwiYSI6ImNqZWo2YXk2bDJwbnozOW83Y3A2bTUxYmkifQ.RFMxc-rS7DhCPJvlaTQUTA";
 
 //Add new layer for all plots
-var allConcerts = new L.LayerGroup();
+//var allConcerts = new L.LayerGroup();
 
 //Add new layer for cluster map
 var clusters = new L.LayerGroup();
@@ -11,7 +11,7 @@ var heatMap = new L.LayerGroup();
 
 // Create overlay object to hold our overlay layer
 var overlayMaps = {
-  "All Concerts": allConcerts,
+  //"All Concerts": allConcerts,
   "Concert Clusters": clusters,
   "Heatmap": heatMap,
 };
@@ -19,7 +19,7 @@ var overlayMaps = {
 var myMap = L.map("map", {
   center: [37.09, -95.71],
   zoom: 3,
-  layers: [allConcerts]
+  layers: [clusters]
 });
 
 var baseMap = L.tileLayer(mapbox).addTo(myMap);
@@ -28,8 +28,8 @@ var baseMap = L.tileLayer(mapbox).addTo(myMap);
 d3.json("/maps", function (error, response) {
   if (error) return console.warn(error);
   // eventdata = JSON.parse(response);
-//Clustermap
-// Grabbing the data with d3..
+  //Clustermap
+  // Grabbing the data with d3..
   console.log(response)
   // Creating a new marker cluster group
   var markers = L.markerClusterGroup();
@@ -39,68 +39,65 @@ d3.json("/maps", function (error, response) {
 
     // Add a new marker to the cluster group and bind a pop-up
     markers.addLayer(L.marker([response[i].Latitude, response[i].Longitude])
-      .bindPopup(response[i].Venue))
+        .bindPopup(response[i].Venue))
       .addTo(clusters);
   };
 
-  function getIcon(genre){
-    if(genre =="Rock"){
+  /* function getIcon(genre) {
+    if (genre == "Rock") {
       console.log(genre);
-        return '../static/images/a6cee3.png';
-  
-    }
-    else if(genre == "Pop"){
-        return '../static/images/1f78b4.png';
-    }
-    else if(genre == "Metal"){
-        return '../static/images/b2df8a.png';
-    }
-    else if(genre =="Hip-Hop/Rap"){
-        return '../static/images/33a02c.png';
-    }
-    else if(genre =="Dance/Electronic"){
-        return '../static/images/fb9a99.png';
-    }
-    else if(genre =="Country"){
-        return '../static/images/e31a1c.png';
-    }
-    else if(genre =="Alternative"){
+      return '../static/images/a6cee3.png';
+
+    } else if (genre == "Pop") {
+      return '../static/images/1f78b4.png';
+    } else if (genre == "Metal") {
+      return '../static/images/b2df8a.png';
+    } else if (genre == "Hip-Hop/Rap") {
+      return '../static/images/33a02c.png';
+    } else if (genre == "Dance/Electronic") {
+      return '../static/images/fb9a99.png';
+    } else if (genre == "Country") {
+      return '../static/images/e31a1c.png';
+    } else if (genre == "Alternative") {
       return '../static/images/fdbf6f.png';
-  }
-    else {
+    } else {
       return '../static/images/a6cee3.png'
     }
   }
 
   for (var i = 0; i < response.length; i++) {
-		var greenIcon = L.icon({
-		    iconUrl: getIcon(response[i].Genre),
-		    iconSize:     [17, 25], // size of the icon
-		    iconAnchor:   [10, 20], // point of the icon which will correspond to marker's location
-		    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-		})
-		L.marker([response[i].Latitude, response[i].Longitude], {icon: greenIcon})
-			.bindPopup("<h3><strong>" + response[i].Name + "</strong></h3> <hr> <h4><strong>Venue: " + response[i].Venue + 
-						"<strong></h4> <hr> <h4><strong>Genre: " + response[i].Genre + "/" + response[i].Subgenre +"<strong></h4>")
-    		.addTo(allConcerts);
+    var greenIcon = L.icon({
+      iconUrl: getIcon(response[i].Genre),
+      iconSize: [17, 25], // size of the icon
+      iconAnchor: [10, 20], // point of the icon which will correspond to marker's location
+      popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+    })
+    L.marker([response[i].Latitude, response[i].Longitude], {
+        icon: greenIcon
+      })
+      .bindPopup("<h3><strong>" + response[i].Name + "</strong></h3> <hr> <h4><strong>Venue: " + response[i].Venue +
+        "<strong></h4> <hr> <h4><strong>Genre: " + response[i].Genre + "/" + response[i].Subgenre + "<strong></h4>")
+      .addTo(allConcerts);
   };
-
+ */
   var heatArray = [];
 
   for (var i = 0; i < response.length; i++) {
     heatArray.push([response[i].Latitude, response[i].Longitude])
-    }
- 
+  }
+
   var heat = L.heatLayer(heatArray, {
     radius: 20,
     blur: 15,
-    gradient: {0.0: 'green',
-               0.5: 'yellow',
-               1.0: 'red'
-              },
+    gradient: {
+      0.0: 'green',
+      0.5: 'yellow',
+      1.0: 'red'
+    },
     minOpacity: 0.7
   }).addTo(heatMap)
+
+  
 });
 
 L.control.layers(overlayMaps).addTo(myMap);
-
